392e4ceb8516579db7d421e6b2bacfc57af5ffa7
jwansek <eddie.atten.ea29@gmail.com>
2020-11-17 21:38:05 +0000

    updated ddl

--- ddl.sql
+++ ddl.sql
@@ -65,6 +65,6 @@ INSERT INTO `chart_colours` VALUES (1, 2, "bar");
 CREATE TABLE IF NOT EXISTS `error_log` (
     `error_id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `status` VARCHAR(15) NOT NULL,
-    `text` VARCHAR(100) NOT NULL,
+    `text` TEXT NOT NULL,
     `datetime` DATETIME NOT NULL DEFAULT NOW()
 );
